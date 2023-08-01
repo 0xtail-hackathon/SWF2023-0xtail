6cdba70ce81dad1c08674683a49cfad09ec4bf49
doyoungkim <doyoungkim0327@gmail.com>
2023-08-01 18:29:22 +0900

    feat: crowdsale remove unused logic, add cap logic

--- contracts/Crowdsale.sol
+++ contracts/Crowdsale.sol
@@ -6,38 +6,32 @@ import "./math/SafeMath.sol";
 
 /**
  * @title Crowdsale
- * @dev Crowdsale is a base contract for managing a token crowdsale,
- * allowing investors to purchase tokens with ether. This contract implements
- * such functionality in its most fundamental form and can be extended to provide additional
- * functionality and/or custom behavior.
- * The external interface represents the basic interface for purchasing tokens, and conform
- * the base architecture for crowdsales. They are *not* intended to be modified / overriden.
- * The internal interface conforms the extensible and modifiable surface of crowdsales. Override
- * the methods to add functionality. Consider using 'super' where appropiate to concatenate
- * behavior.
  */
 contract Crowdsale {
     using SafeMath for uint256;
 
-    // The token being sold -> RT
-    ERC20 public token;
-
     // Address where funds are collected
     address public wallet;
 
-    // How many token units a buyer gets per wei -> 1:1
+    // How many token units a buyer gets per wei
     uint256 public rate;
 
     // Amount of wei raised
     uint256 public weiRaised;
 
-   /**
-   * Event for token purchase logging
-   * @param purchaser who paid for the tokens
-   * @param beneficiary who got the tokens
-   * @param value weis paid for purchase
-   * @param amount amount of tokens purchased
-   */
+    // How much wei account has funded. Address -> wei
+    mapping(address => uint256) public weiMap;
+
+    // Max amount of wei to be contributed
+    uint256 public cap;
+
+    /**
+    * Event for token purchase logging
+    * @param purchaser who paid for the tokens
+    * @param beneficiary who got the tokens
+    * @param value weis paid for purchase
+    * @param amount amount of tokens purchased
+    */
     event TokenPurchase(
         address indexed purchaser,
         address indexed beneficiary,
@@ -48,16 +42,17 @@ contract Crowdsale {
    /**
    * @param _rate Number of token units a buyer gets per wei - 1
    * @param _wallet Address where collected funds will be forwarded to - admin address
-   * @param _token Address of the token being sold
+   * @param _cap Max amount of wei to be contributed
    */
-    constructor(uint256 _rate, address _wallet, ERC20 _token) public {
+    constructor(uint256 _rate, address _wallet, uint256 _cap) public {
         require(_rate > 0);
         require(_wallet != address(0));
-        require(_token != address(0));
+        require(_cap > 0);
 
         rate = _rate;
         wallet = _wallet;
-        token = _token;
+        cap = _cap;
+
     }
 
     // -----------------------------------------
@@ -71,6 +66,31 @@ contract Crowdsale {
         buyTokens(msg.sender);
     }
 
+    /**
+    * @dev Return cap of crowdsale
+    * @return Cap amount
+    */
+    function cap() public view returns (uint256) {
+        return cap;
+    }
+
+    /**
+    * @dev Checks whether the cap has been reached.
+    * @return Whether the cap was reached
+    */
+    function capReached() public view returns (bool) {
+        return weiRaised >= cap;
+    }
+
+    /**
+    * @dev Returns funded amount by the address.
+    * @param buyer Address performed funding
+    * @return funded amount of the address
+    */
+    function fundAmount(address buyer) public view returns (uint256) {
+        return weiMap[buyer];
+    }
+
    /**
    * @dev low level token purchase ***DO NOT OVERRIDE***
    * @param _beneficiary Address performing the token purchase
@@ -81,23 +101,19 @@ contract Crowdsale {
         _preValidatePurchase(_beneficiary, weiAmount);
 
         // calculate token amount to be created
-        uint256 tokens = _getTokenAmount(weiAmount);
+        uint256 amount = _getTokenAmount(weiAmount);
 
         // update state
         weiRaised = weiRaised.add(weiAmount);
 
-        _processPurchase(_beneficiary, tokens);
+        _processPurchase(_beneficiary, amount);
         emit TokenPurchase(
             msg.sender,
             _beneficiary,
             weiAmount,
-            tokens
+            amount
         );
 
-        _updatePurchasingState(_beneficiary, weiAmount);
-
-        _forwardFunds();
-        _postValidatePurchase(_beneficiary, weiAmount);
     }
 
     // -----------------------------------------
@@ -117,21 +133,9 @@ contract Crowdsale {
     {
         require(_beneficiary != address(0));
         require(_weiAmount != 0);
+        require(weiRaised.add(_weiAmount) <= cap, "cap reached");
     }
 
-   /**
-   * @dev Validation of an executed purchase. Observe state and use revert statements to undo rollback when valid conditions are not met.
-   * @param _beneficiary Address performing the token purchase
-   * @param _weiAmount Value in wei involved in the purchase
-   */
-    function _postValidatePurchase(
-        address _beneficiary,
-        uint256 _weiAmount
-    )
-    internal
-    {
-        // optional override
-    }
 
    /**
    * @dev Source of tokens. Override this method to modify the way in which the crowdsale ultimately gets and sends its tokens.
@@ -144,7 +148,10 @@ contract Crowdsale {
     )
     internal
     {
-        token.transfer(_beneficiary, _tokenAmount);
+
+        uint256 before = weiMap[_beneficiary];
+        uint256 newAmount = before.add(_tokenAmount);
+        weiMap[_beneficiary] = newAmount;
     }
 
    /**
@@ -162,20 +169,6 @@ contract Crowdsale {
     }
 
    /**
-   * @dev Override for extensions that require an internal state to check for validity (current user contributions, etc.)
-   * @param _beneficiary Address receiving the tokens
-   * @param _weiAmount Value in wei involved in the purchase
-   */
-    function _updatePurchasingState(
-        address _beneficiary,
-        uint256 _weiAmount
-    )
-    internal
-    {
-        // optional override
-    }
-
-   /**
    * @dev Override to extend the way in which ether is converted to tokens.
    * @param _weiAmount Value in wei to be converted into tokens
    * @return Number of tokens that can be purchased with the specified _weiAmount
@@ -186,10 +179,4 @@ contract Crowdsale {
         return _weiAmount.mul(rate);
     }
 
-   /**
-   * @dev Determines how ETH is stored/forwarded on purchases.
-   */
-    function _forwardFunds() internal {
-        wallet.transfer(msg.value);
-    }
 }
