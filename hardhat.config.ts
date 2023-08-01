d3dd9d76e1dfa19669a7f500b6e759b83f3457f1
doyoungkim <doyoungkim0327@gmail.com>
2023-08-01 13:52:54 +0900

    feat: RootToken, Crowdsale impl

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,18 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import '@nomiclabs/hardhat-waffle';
+import 'hardhat-gas-reporter';
+import {DEPLOYER_PRIVATE_KEY} from "./constants";
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+
+const config = {
+  solidity: "0.4.23",
+  networks: {
+    cronosTestnet: {
+      url: 'https://evm-t3.cronos.org',
+      chainId: 338,
+      accounts: [DEPLOYER_PRIVATE_KEY],
+      gasPrice: 'auto',
+    },
+  }
 };
 
 export default config;
